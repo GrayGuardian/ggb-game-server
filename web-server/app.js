882126af36bd117934ee755c0debe55e5415cbb1
pc <pc@qq.com>
2021-01-29 10:14:10 +0800

    添加web-server；

---
+++ web-server/app.js
@@ -0,0 +1,16 @@
+require('../common/global');
+const Koa = require('koa');
+const static = require('koa-static');
+const app = new Koa();
+
+console.log('Server Start>>>', `Type:${SERVER_TYPE} Name:${SERVER_NAME} Url:${SERVER_URL} Port:${SERVER_PORT}`);
+
+//http
+app.use(static(__dirname + '/public'));
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Http Server Start >>> http://${SERVER_URL}:${SERVER_PORT}`);
+});
+
+//socket.io
+global.center_mgr = require('../common/center_mgr')();
