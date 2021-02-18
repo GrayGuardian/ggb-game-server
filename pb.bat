b5e14500b1d13c5bbe202f19402d1b0cd5b293a9
pc <pc@qq.com>
2021-02-18 16:38:46 +0800

    添加pb导出工具类；

---
+++ pb.bat
@@ -0,0 +1,31 @@
+@echo off
+set exedir=%cd%\tool\protoc-gen-lua\protobuf-lua\
+set prodir=%cd%\protocol
+set builddir=D:\Demo\Unity\Game\koa-game-client\Assets\Resources\AB\lua\pb
+
+
+::生成语句
+set code=--plugin=protoc-gen-lua="%exedir%\plugin\protoc-gen-lua.bat" --lua_out=%builddir% -I %prodir% 
+
+::遍历procotol所在子文件夹
+for /f "delims=" %%i in ('dir /ad/b/s "%prodir%"') do (
+setlocal enabledelayedexpansion
+set var=%%i
+::echo !var!
+set code=!code!-I !var! 
+)
+
+echo %code%
+::导出所有procotol
+for /R %prodir% %%f in (*.proto) do ( 
+setlocal enabledelayedexpansion
+set var=%%f
+echo Build Procotol File:!var!
+
+%exedir%protoc.exe !code!!var!
+
+)
+
+
+
+pause
